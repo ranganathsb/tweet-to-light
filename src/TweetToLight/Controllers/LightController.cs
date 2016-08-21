e618f8391299bf28d9836361e0236a9e0bcaffd9
Sharon Cichelli <girlwritescode@gmail.com>
2016-08-21 16:57:51 -0500

    Hello, World!

---
+++ src/TweetToLight/Controllers/LightController.cs
@@ -0,0 +1,12 @@
+﻿using System.Web.Http;
+
+namespace TweetToLight.Controllers
+{
+    public class LightController : ApiController
+    {
+        public string Get()
+        {
+            return "Hello, World";
+        }
+    }
+}
