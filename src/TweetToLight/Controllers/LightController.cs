19b8f1e59676984b20c6391b70c676aa90779efb
Sharon Cichelli <girlwritescode@gmail.com>
2016-08-28 17:00:43 -0500

    create a POST endpoint to receive the text of the tweet to parse, and turn it into RGB values to send back to the LED

--- src/TweetToLight/Controllers/LightController.cs
+++ src/TweetToLight/Controllers/LightController.cs
@@ -8,5 +8,22 @@ namespace TweetToLight.Controllers
         {
             return "Hello, World";
         }
+
+        public RgbResponse Post(RgbRequest request)
+        {
+            return new RgbResponse { Red = 255, Green = 255, Blue = 255 };
+        }
+    }
+
+    public class RgbResponse
+    {
+        public int Red { get; set; }
+        public int Green { get; set; }
+        public int Blue { get; set; }
+    }
+
+    public class RgbRequest
+    {
+        public string Tweet { get; set; }
     }
 }
